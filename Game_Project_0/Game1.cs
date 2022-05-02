dc0c476f2be2bec81cdeec48757ca4819494597f
Daniel Koutris <dangvk3@gmail.com>
2022-05-01 19:04:44 -0500

    Add project files.

---
+++ Game_Project_0/Game1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game_Project_0
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private imageEntity bacon;
+        private imageEntity ghost;
+        private imageEntity sword;
+        private imageEntity us;
+
+        public int gametick;
+
+        SpriteFont spriteFont;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            bacon = new imageEntity(this, Color.White, "bacon") { Position = new Vector2(0, 20) };
+            ghost = new imageEntity(this, Color.White, "ghost") { Position = new Vector2(400, 100) };
+            sword = new imageEntity(this, Color.White, "sword") { Position = new Vector2(100, 300) };
+            us = new imageEntity(this, Color.White, "us") { Position = new Vector2(200, 20) };
+
+            gametick = 0;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            bacon.LoadContent();
+            ghost.LoadContent();
+            sword.LoadContent();
+            us.LoadContent();
+
+            // TODO: use this.Content to load your game content here
+            spriteFont = Content.Load<SpriteFont>("Font1");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+
+            gametick++;
+
+            if (gametick > 500) gametick = 0;
+            bacon.Update(gametick);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            bacon.Draw(_spriteBatch, gametick);
+            ghost.Draw(_spriteBatch, gametick);
+            sword.Draw(_spriteBatch, gametick);
+            us.Draw(_spriteBatch, gametick);
+            _spriteBatch.DrawString(spriteFont, "Press Escape To Get Out Of Here!", new Vector2(300, 300), Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Game_Project_0/imageEntity.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game_Project_0
+{
+    public class imageEntity
+    {
+        Random random;
+
+        /// <summary>
+        /// The game this ball is a part of
+        /// </summary>
+        Game game;
+
+        /// <summary>
+        /// A color to help distinguish one ball from another
+        /// </summary>
+        Color color;
+
+        /// <summary>
+        /// The texture to apply to a ball
+        /// </summary>
+        Texture2D texture;
+
+        /// <summary>
+        /// The position of the ball in the game world
+        /// </summary>
+        public Vector2 Position { get; set; }
+
+        string textureName;
+
+        /// <summary>
+        /// Constructs a new ball instance
+        /// </summary>
+        /// <param name="game">The game this ball belongs in</param>
+        /// <param name="color">A color to distinguish this ball</param>
+        public imageEntity(Game game, Color color, string Name)
+        {
+            this.game = game;
+            this.color = color;
+            this.random = new Random();
+            textureName = Name;
+        }
+
+        // Helper for reading input from keyboard, gamepad, and touch input. This class 
+        // tracks both the current and previous state of the input devices, and implements 
+        // query methods for high level input actions such as "move up through the menu"
+        // or "pause the game".
+
+
+        /// <summary>
+        /// Loads the ball's texture
+        /// </summary>
+        public void LoadContent()
+        {
+            texture = game.Content.Load<Texture2D>(textureName);
+        }
+
+        public void Update(int gametick)
+        {
+            //if(gametick > 500)
+        }
+
+        /// <summary>
+        /// Draws the ball at its current position and with 
+        /// its assigned color
+        /// </summary>
+        /// <param name="spriteBatch">The SpriteBatch to render with</param>
+        public void Draw(SpriteBatch spriteBatch, int gametick)
+        {
+            if (texture is null) throw new InvalidOperationException("Texture must be loaded to render");
+
+            if (textureName == "bacon")
+            {
+                if (gametick < 250)
+                {
+                    spriteBatch.Draw(texture, new Rectangle(0, 20, 200, 200), null, Color.White, 0, new Vector2(0, 0), SpriteEffects.None, 0f);
+                }
+                else
+                {
+                    spriteBatch.Draw(texture, new Rectangle(0, 20, 200, 200), null, Color.White, 0, new Vector2(0, 0), SpriteEffects.FlipHorizontally, 0f);
+                }
+                
+
+                
+            }
+            else
+            {
+                spriteBatch.Draw(texture, Position, color);
+            }
+            
+        }
+
+    }
+}
